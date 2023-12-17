ea92f98a17ffc664d76965f9ae1d7b9a7d4f70b2
Mehdi-Safraoui <mahdisafraoui01@gmail.com>
2023-12-17 10:47:27 +0100

    3éme modification (Authentification + firebase)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import SignUp from "./components/signup";
-import { ToastContainer } from 'react-toastify';
+import Login from "./components/login";
 import 'react-toastify/dist/ReactToastify.css';
 
 
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <>
       <SignUp />
-      <ToastContainer />
+      <Login />  
     </>
   );
 };
